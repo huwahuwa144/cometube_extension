d3b7dc8bc0d6423c416ce3075d687541deb33e41
sadarukun <pnt.so.krn22@gmail.com>
2018-09-29 01:45:36 +0900

    たぶはでた

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,5 +1,19 @@
 'use strict';
 
-$(function(){
-  $('body').css('background-color','#ff0000');
+// $(function(){
+//   $('body').css('background-color','#ff0000');
+// });
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.create({'url': 'index.html' });
 });
+
+
+// var url = chrome.extension.getURL('index.html');
+//
+// $(function() {
+//   $('#demo1').click(function(){
+//       window.open('url');
+//       return false;
+//     });
+// });

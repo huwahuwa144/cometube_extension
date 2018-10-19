fd443468fe8bd27b9075a1dbd4045503e7a002bb
Yhara <ikomatech@gmail.com>
2018-10-19 13:57:21 +0900

    コメントの全件取得成功、htmlのひな型作成

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,15 +1,51 @@
-chrome.identity.getAuthToken({
-    interactive: true
-}, function(token) {
-    if (chrome.runtime.lastError) {
-        alert(chrome.runtime.lastError.message);
-        return;
+//pageToken格納用変数
+var nextPageToken = '';
+var token = '';
+var allComments = [];
+
+/**
+ * [commentThreadsApi 指定したvideoIdのコメントid一覧を取得]
+ * @param  {[String]} v [videoId]
+ * @param  {[String]} t [token]
+ * @return {[type]}   [description]
+ */
+function commentThreadsApi(v,t,n,callback) {
+  gapi.client.load('youtube', 'v3', function() {
+    var request = gapi.client.youtube.commentThreads.list({
+      videoId: v,
+      part: 'snippet',
+      access_token: t,
+      pageToken: n
+    });
+    request.execute(function(response) {
+      callback(response,response.nextPageToken,v,t);
+    });
+  });
+}
+
+
+function getAllComments(json,n,v,t){
+    console.log('response:', json);
+    console.log(nextPageToken);
+    allComments.push(json)
+    if('nextPageToken' in json){
+      commentThreadsApi(v,t,n,getAllComments);
     }
-    var x = new XMLHttpRequest();
-    x.open('GET', 'https://www.googleapis.com/oauth2/v1/tokeninfo?alt=json&access_token=' + token);
-    x.onload = function() {
-        alert(x.response);
-        console.log(token);
-    };
-    x.send();
+    console.log(allComments[4]);
+}
+
+chrome.identity.getAuthToken({
+  interactive: true
+}, function(token) {
+  if (chrome.runtime.lastError) {
+    alert(chrome.runtime.lastError.message);
+    return;
+  }
+  var x = new XMLHttpRequest();
+  x.open('GET', 'https://www.googleapis.com/oauth2/v1/tokeninfo?alt=json&access_token=' + token);
+  x.onload = function() {
+    commentThreadsApi('OM1c-fc93xY',token,nextPageToken,getAllComments);
+    console.log(nextPageToken);
+  };
+  x.send();
 });

ccdfc0ce751ee112606ed4f4bbd7aea7387135b5
MAETEL\2160072 <pnt.so.krn22@gmail.com>
2018-11-21 14:48:38 +0900

    改変

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,7 +1,10 @@
+'use strict';
+
 //pageToken格納用変数
 var nextPageToken = '';
 var token = '';
 var allComments = [];
+var flg = 1;
 
 /**
  * [commentThreadsApi 指定したvideoIdのコメントid一覧を取得]
@@ -9,16 +12,16 @@ var allComments = [];
  * @param  {[String]} t [token]
  * @return {[type]}   [description]
  */
-function commentThreadsApi(v,t,n,callback) {
-  gapi.client.load('youtube', 'v3', function() {
+function commentThreadsApi(v, t, n, callback) {
+  gapi.client.load('youtube', 'v3', function () {
     var request = gapi.client.youtube.commentThreads.list({
       videoId: v,
       part: 'snippet',
       access_token: t,
       pageToken: n
     });
-    request.execute(function(response) {
-      callback(response,response.nextPageToken,v,t);
+    request.execute(function (response) {
+      callback(response, response.nextPageToken, v, t);
     });
   });
 }
@@ -31,50 +34,48 @@ function commentThreadsApi(v,t,n,callback) {
  * @param  {[type]} t    [access_token]
  * @return {[type]}      [description]
  */
-function getAllComments(json,n,v,t){
-    console.log('response:', json);
-    console.log(nextPageToken);
-    allComments.push(json)
-    if('nextPageToken' in json){
-      commentThreadsApi(v,t,n,getAllComments);
+function getAllComments(json, n, v, t) {
+  console.log('response:', json);
+  console.log(nextPageToken);
+  allComments.push(json);
+  if ('nextPageToken' in json) {
+    commentThreadsApi(v, t, n, getAllComments);
+  }
+  console.log(allComments[4]);
+}
+
+
+ //ポップアップとログインの表示
+function startExecute() {
+  var currentTab = '';
+  chrome.tabs.getSelected(null, function (tab) {
+    currentTab = tab.url;
+    var id = /[/?=]([-\w]{11})/.exec(tab.url);
+    console.log(id);
+    alert(tab.title + '\n' + tab.url);
+    if (id != null) {
+      chrome.identity.getAuthToken({
+        interactive: true
+      }, function (token) {
+        if (chrome.runtime.lastError) {
+          alert(chrome.runtime.lastError.message);
+          return;
+        }
+        var x = new XMLHttpRequest();
+        x.open('GET', 'https://www.googleapis.com/oauth2/v1/tokeninfo?alt=json&access_token=' + token);
+        x.onload = function () {
+          commentThreadsApi(id[1], token, nextPageToken, getAllComments);
+          console.log(nextPageToken);
+          flg = 0;
+        };
+        x.send();
+        if(flg == 0){
+          //reply html に飛ばす
+            chrome.tabs.create({'url': 'reply.html' });
+        }
+      });
     }
-    console.log(allComments[4]);
-}
-
-/**
- * [startExecute description]
- * @return {[type]} [description]
- */
- //
-function startExecute(){
-  var currentTab = ''
-  chrome.tabs.getSelected(null, function(tab) {
-      currentTab = tab.url;
-      var id = /[/?=]([-\w]{11})/.exec(tab.url);
-      console.log(id);
-      if(id !=null){
-        chrome.identity.getAuthToken({
-          interactive: true
-        }, function(token) {
-          if (chrome.runtime.lastError) {
-            alert(chrome.runtime.lastError.message);
-            return;
-          }
-          var x = new XMLHttpRequest();
-          x.open('GET', 'https://www.googleapis.com/oauth2/v1/tokeninfo?alt=json&access_token=' + token);
-          x.onload = function() {
-            commentThreadsApi(id[1],token,nextPageToken,getAllComments);
-            console.log(nextPageToken);
-          };
-          console.log(allComments[0]);
-          x.send();
-        });
-      }
   });
-}
-
-function commentListSet(json){
-
 }
 
 chrome.browserAction.onClicked.addListener(startExecute);

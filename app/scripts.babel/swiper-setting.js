685df7b75b74d7f11d7f9dae6bd36782bab8addf
Yhara <ikomatech@gmail.com>
2019-01-10 13:51:52 +0900

    0110授業前

--- app/scripts.babel/swiper-setting.js
+++ app/scripts.babel/swiper-setting.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 'use strict';
 
 var swiper = new Swiper('.swiper-container', {
@@ -11,15 +11,3 @@ var swiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev'
   }
 });
-=======
-var swiper = new Swiper('.swiper-container', {
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'progressbar',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
->>>>>>> 9914dbe698504d727333bb4ccac3e0ca0d3f2a30
--- app/scripts.babel/youtuber.js
+++ app/scripts.babel/youtuber.js
@@ -1,3 +1,5 @@
+'use strict';
+
 //pageToken格納用変数
 var nextPageToken = '';
 var token = '';
@@ -5,20 +7,24 @@ var allComments = [];
 var flg = 1;
 var title = '';
 var id;
-chrome.runtime.onMessage.addListener(
-	function(request,sender,sendResponse){
-		parseItems = [];
-		console.log(sender);
-    id = sender.videoId;
-		var res = 'finish';
-		sendResponse(res);
+var userIcon;
+var userName;
+var userComment;
+var userPublishedAt;
+var commentCount;
+var commentCounter;
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  parseItems = [];
+  console.log(sender);
+  id = sender.videoId;
+  var res = 'finish';
+  sendResponse(res);
+});
 
-
-	}
-);
 id = localStorage.id;
 title = localStorage.videoTitle;
 console.log(id);
+
 chrome.identity.getAuthToken({
   interactive: true
 }, function (token) {
@@ -31,15 +37,40 @@ chrome.identity.getAuthToken({
   x.onload = function () {
     commentThreadsApi(id, token, nextPageToken, getAllComments);
   };
-  x.onloadend = function(){
-
-  }
+  x.onloadend = function () {};
   x.send();
-  // if(flg == 0){
-  //   //reply html に飛ばす
-
-  // }
 });
+
+/**
+ * [getAllComments description]
+ * @param  {[type]} json [commentInfo]
+ * @param  {[type]} n    [nextPageToken]
+ * @param  {[type]} v    [videoId]
+ * @param  {[type]} t    [access_token]
+ * @return {[type]}      [description]
+ */
+function getAllComments(json, n, v, t) {
+  console.log('response:', json);
+  console.log(nextPageToken);
+  allComments.push(json);
+  if ('nextPageToken' in json) {
+    commentThreadsApi(v, t, n, getAllComments);
+  } else {
+    document.getElementById('movieTitle').innerText = title;
+    userIcon = allComments[0].items[0].snippet.topLevelComment.snippet.authorProfileImageUrl
+    userName = allComments[0].items[0].snippet.topLevelComment.snippet.authorDisplayName
+    userComment = allComments[0].items[0].snippet.topLevelComment.snippet.textDisplay
+    userPublishedAt = allComments[0].items[0].snippet.topLevelComment.snippet.updatedAt
+
+    for (var i = 0; i < allComments.length; i++) {
+      for (var j = 0; j < allComments[i].items.length; j++) {
+        commentCount+=1;
+      }
+    }
+    $('<div class="swiper-slide"><div class="list-group"><a href="#" class="list-group-item list-group-item-action flex-column align-items-start">	<div class="d-flex w-100 justify-content-between"><img class="d-block img-fluid rounded-circle float-left" src='+ userIcon +' style="	width: 80px;	height: 80px;">	<h2 class="mb-1 flex-row justify-content-start align-items-end flex-grow-1 d-flex mx-2" style="">'+userName+' </h2> <small class="text-muted">'+publishedAt+'</small></div><h3 class="my-2">'+userComment+'</h3></a></div></div>').appendTo('#swiper-wrapper');
+    console.log(title);
+  }
+}
 
 /**
  * [commentThreadsApi 指定したvideoIdのコメントid一覧を取得]
@@ -61,36 +92,19 @@ function commentThreadsApi(v, t, n, callback) {
   });
 }
 
-/**
- * [getAllComments description]
- * @param  {[type]} json [commentInfo]
- * @param  {[type]} n    [nextPageToken]
- * @param  {[type]} v    [videoId]
- * @param  {[type]} t    [access_token]
- * @return {[type]}      [description]
- */
-function getAllComments(json, n, v, t) {
-  console.log('response:', json);
-  console.log(nextPageToken);
-  allComments.push(json);
-  if ('nextPageToken' in json) {
-    commentThreadsApi(v, t, n, getAllComments);
-  }else{
-    document.getElementById('movieTitle').innerText = title;
-		for(let i = 0 ;i < allComments.length;i++){
-			console.log(allComments[i]);
-			for(let j = 0;j < 1;i++){
-					$('<div class="swiper-slide"><div class="list-group"><a href="#" class="list-group-item list-group-item-action flex-column align-items-start">	<div class="d-flex w-100 justify-content-between"><img class="d-block img-fluid rounded-circle float-left" src="https://pingendo.com/assets/photos/wireframe/photo-1.jpg" style="	width: 80px;	height: 80px;">	<h2 class="mb-1 flex-row justify-content-start align-items-end flex-grow-1 d-flex mx-2" style="">ユーザー名 </h2> <small class="text-muted">コントの時間表示</small></div><h3 class="my-2">ここにyoutubeのコメント</h3></a></div></div>').appendTo('#swiper-wrapper-id');
-			}
-		}
-    console.log(title);
-  }
-}
+function commentSend(token,id,text) {
+  gapi.client.load('youtube', 'v3', function () {
+    var request = gapi.client.youtube.comments.insert({
+      part:'snippet',
+      access_token:token,
+    },{
+      snippet:{
+        parentId:id,
+        textOriginal:text
+      }
+    });
+    request.execute(function(response){
 
-function commentSend(){
-	gapi.client.load('youtube','v3',function(){
-		var request = gapi.client.youtube.comments.insert({
-
-		})
-	});
+    })
+  });
 }

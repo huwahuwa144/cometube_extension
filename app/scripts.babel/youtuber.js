5055b0447a7c2fb91224d5f2d99bc6c7f110ea73
Yhara <ikomatech@gmail.com>
2019-01-10 19:01:55 +0900

    基本動作完成

--- app/scripts.babel/youtuber.js
+++ app/scripts.babel/youtuber.js
@@ -2,7 +2,7 @@
 
 //pageToken格納用変数
 var nextPageToken = '';
-var token = '';
+var tokenResult;
 var allComments = [];
 var flg = 1;
 var title = '';
@@ -11,8 +11,11 @@ var userIcon;
 var userName;
 var userComment;
 var userPublishedAt;
-var commentCount;
-var commentCounter;
+var commentCount = 0;
+var commentCounter = 0;
+var realCommentId;
+var inputText;
+var s;
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   parseItems = [];
   console.log(sender);
@@ -28,6 +31,7 @@ console.log(id);
 chrome.identity.getAuthToken({
   interactive: true
 }, function (token) {
+  tokenResult = token;
   if (chrome.runtime.lastError) {
     alert(chrome.runtime.lastError.message);
     return;
@@ -57,17 +61,18 @@ function getAllComments(json, n, v, t) {
     commentThreadsApi(v, t, n, getAllComments);
   } else {
     document.getElementById('movieTitle').innerText = title;
-    userIcon = allComments[0].items[0].snippet.topLevelComment.snippet.authorProfileImageUrl
-    userName = allComments[0].items[0].snippet.topLevelComment.snippet.authorDisplayName
-    userComment = allComments[0].items[0].snippet.topLevelComment.snippet.textDisplay
-    userPublishedAt = allComments[0].items[0].snippet.topLevelComment.snippet.updatedAt
-
+    userIcon = allComments[0].items[0].snippet.topLevelComment.snippet.authorProfileImageUrl;
+    userName = allComments[0].items[0].snippet.topLevelComment.snippet.authorDisplayName;
+    userComment = allComments[0].items[0].snippet.topLevelComment.snippet.textDisplay;
+    userPublishedAt = allComments[0].items[0].snippet.topLevelComment.snippet.updatedAt;
+    realCommentId = allComments[0].items[0].id;
     for (var i = 0; i < allComments.length; i++) {
       for (var j = 0; j < allComments[i].items.length; j++) {
         commentCount+=1;
       }
     }
-    $('<div class="swiper-slide"><div class="list-group"><a href="#" class="list-group-item list-group-item-action flex-column align-items-start">	<div class="d-flex w-100 justify-content-between"><img class="d-block img-fluid rounded-circle float-left" src='+ userIcon +' style="	width: 80px;	height: 80px;">	<h2 class="mb-1 flex-row justify-content-start align-items-end flex-grow-1 d-flex mx-2" style="">'+userName+' </h2> <small class="text-muted">'+publishedAt+'</small></div><h3 class="my-2">'+userComment+'</h3></a></div></div>').appendTo('#swiper-wrapper');
+    commentCounter = 1;
+    $('<div class="swiper-slide"><div class="list-group"><a href="#" class="list-group-item list-group-item-action flex-column align-items-start">	<div class="d-flex w-100 justify-content-between"><img class="d-block img-fluid rounded-circle float-left" src='+ userIcon +' style="	width: 80px;	height: 80px;">	<h2 class="mb-1 flex-row justify-content-start align-items-end flex-grow-1 d-flex mx-2" style="">'+userName+' </h2> <small class="text-muted">'+userPublishedAt+'</small></div><h3 class="my-2">'+userComment+'</h3></a></div></div>').appendTo('#swiper-wrapper');
     console.log(title);
   }
 }
@@ -92,19 +97,54 @@ function commentThreadsApi(v, t, n, callback) {
   });
 }
 
-function commentSend(token,id,text) {
+function commentSend() {
+  console.log(inputText);
   gapi.client.load('youtube', 'v3', function () {
     var request = gapi.client.youtube.comments.insert({
       part:'snippet',
-      access_token:token,
+      access_token:tokenResult,
     },{
       snippet:{
-        parentId:id,
-        textOriginal:text
+        parentId:realCommentId,
+        textOriginal:document.getElementById('form35').value
       }
     });
     request.execute(function(response){
-
+      console.log(response);
     })
   });
+}
+
+function commentNext(){
+  var k = Math.floor(commentCounter/20);
+  var l = Math.floor(commentCounter%20);
+  if(commentCounter<commentCount){
+    userIcon = allComments[k].items[l].snippet.topLevelComment.snippet.authorProfileImageUrl;
+    userName = allComments[k].items[l].snippet.topLevelComment.snippet.authorDisplayName;
+    userComment = allComments[k].items[l].snippet.topLevelComment.snippet.textDisplay;
+    userPublishedAt = allComments[k].items[l].snippet.topLevelComment.snippet.updatedAt;
+    realCommentId = allComments[k].items[l].id;
+    document.getElementById('swiper-wrapper').textContent = null;
+    $('<div class="swiper-slide"><div class="list-group"><a href="#" class="list-group-item list-group-item-action flex-column align-items-start">	<div class="d-flex w-100 justify-content-between"><img class="d-block img-fluid rounded-circle float-left" src='+ userIcon +' style="	width: 80px;	height: 80px;">	<h2 class="mb-1 flex-row justify-content-start align-items-end flex-grow-1 d-flex mx-2" style="">'+userName+' </h2> <small class="text-muted">'+userPublishedAt+'</small></div><h3 class="my-2">'+userComment+'</h3></a></div></div>').appendTo('#swiper-wrapper');
+    commentCounter+=1;
+  }
+  console.log(k);
+  console.log(l);
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('next-comment').addEventListener('click', commentNext);
+});
+
+document.addEventListener('DOMContentLoaded',function(){
+  document.getElementById('send-click').addEventListener('click',commentSend);
+});
+
+document.addEventListener('DOMContentLoaded',function(){
+  document.getElementById('form35').addEventListener('input',print);
+});
+
+function print(){
+  inputText = document.getElementById('form35').value;
 }
